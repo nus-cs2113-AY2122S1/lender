b159738c2687b2e60ab1b79438deeff7197a2f76
Akshay Narayan <okkhoy@gmail.com>
2021-08-20 15:40:03 +0800

    Add loan class

---
+++ src/main/java/Loan.java
@@ -0,0 +1,33 @@
+public class Loan {
+    private String borrower;
+    private int amount;
+    private boolean isPaid;
+    private double interestRate;
+
+    public Loan(String borrower, int amount, double interestRate) {
+        this.borrower = borrower;
+        this.amount = amount;
+        this.interestRate = interestRate;
+        this.isPaid = false;
+    }
+
+    public void setAsPaid() {
+        isPaid = true;
+    }
+
+    public boolean isPaid() {
+        return isPaid;
+    }
+
+    public String getBorrower() {
+        return borrower;
+    }
+
+    public String getDisplayString() {
+        return borrower + ": " + amount;
+    }
+
+    public double getProfit() {
+        return amount * interestRate;
+    }
+}
