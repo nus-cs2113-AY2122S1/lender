9246b3fd3da43a235314c4e428786849543b6c9c
Akshay Narayan @ HP360 (MyOfficeThing) <okkhoy@gmail.com>
2021-08-20 11:30:24 +0800

    Add support to track interest rate and calculate profit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,11 +2,13 @@ public class Main {
     static int[] amounts = new int[100];
     static String[] borrowers = new String[100];
     static boolean[] isPaid = new boolean[100];
+    static double[] interestRates = new double[100];
     static int loansCount = 0;
 
-    public static void addLoan(String borrower, int amount){
+    public static void addLoan(String borrower, int amount, double interestRate){
         borrowers[loansCount] = borrower;
         amounts[loansCount] = amount;
+        interestRates[loansCount] = interestRate;
         loansCount++;
     }
 
@@ -26,11 +28,23 @@ public class Main {
         }
     }
 
+    public static double calculateProfitEarned(){
+        double profit = 0;
+        for (int i=0; i< loansCount; i++){
+            // if the loan was repaid, add the interest to profit
+            if (isPaid[i]){
+                profit = profit + (amounts[i] * interestRates[i]);
+            }
+        }
+        return profit;
+    }
+
     public static void main(String[] args) {
-        addLoan("Alice Betsy", 10);
-        addLoan("Ben Chee", 20);
-        addLoan("Chris Davis", 30);
+        addLoan("Alice Betsy", 10, 0.1);
+        addLoan("Ben Chee", 20, 0.2);
+        addLoan("Chris Davis", 30, 0.3);
         setAsPaid("Ben Chee");
         listLoans();
+        System.out.println("profit: " + calculateProfitEarned());
     }
 }
