5b4eb150872bb713a4e484b53046be7af78bc216
Akshay Narayan @ HP360 (MyOfficeThing) <okkhoy@gmail.com>
2021-08-20 11:26:45 +0800

    Support tracking borrowers

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,23 @@
 public class Main {
-    static int[] loans = new int[100];
+    static int[] amounts = new int[100];
+    static String[] borrowers = new String[100];
     static int loansCount = 0;
 
-    public static void addLoan(int amount){
-        loans[loansCount] = amount;
+    public static void addLoan(String borrower, int amount){
+        borrowers[loansCount] = borrower;
+        amounts[loansCount] = amount;
         loansCount++;
     }
 
     public static void listLoans(){
         for (int i=0; i< loansCount; i++){
-            System.out.println(loans[i]);
+            System.out.println(borrowers[i] + ": " + amounts[i]);
         }
     }
 
     public static void main(String[] args) {
-        addLoan(10);
-        addLoan(20);
+        addLoan("Alice Betsy", 10);
+        addLoan("Ben Chee", 20);
         listLoans();
     }
 }
