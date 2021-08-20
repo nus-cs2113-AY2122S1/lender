f64d1326e577681c47df2baef3cbd930d0ffd6ee
Akshay Narayan @ HP360 (MyOfficeThing) <okkhoy@gmail.com>
2021-08-20 11:28:21 +0800

    Support tracking paid loans status

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     static int[] amounts = new int[100];
     static String[] borrowers = new String[100];
+    static boolean[] isPaid = new boolean[100];
     static int loansCount = 0;
 
     public static void addLoan(String borrower, int amount){
@@ -9,15 +10,27 @@ public class Main {
         loansCount++;
     }
 
+    public static void setAsPaid(String borrower){
+        for (int i=0; i< loansCount; i++){
+            if (borrowers[i].equals(borrower)){
+                isPaid[i] = true;
+            }
+        }
+    }
+
     public static void listLoans(){
         for (int i=0; i< loansCount; i++){
-            System.out.println(borrowers[i] + ": " + amounts[i]);
+            if (!isPaid[i]){
+                System.out.println(borrowers[i] + ": " + amounts[i]);
+            }
         }
     }
 
     public static void main(String[] args) {
         addLoan("Alice Betsy", 10);
         addLoan("Ben Chee", 20);
+        addLoan("Chris Davis", 30);
+        setAsPaid("Ben Chee");
         listLoans();
     }
 }
